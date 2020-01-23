7e081cedff6c5dd1a53c71a30677b9c4cfe33261
deepakgouda <deepakgouda1729@gmail.com>
2020-01-23 11:13:43 +0000

    Add correct recursive function

--- MatrixMultiplication/optimization.cpp
+++ MatrixMultiplication/optimization.cpp
@@ -10,18 +10,18 @@ depending on cache size.
 Optimization 3 : Recursive method.
 */
 
-#include<iostream>
-#include<iomanip>
-#include<stdlib.h>
-#include<fstream>
-#include<cstring>
-#include<time.h>
-#include<math.h>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <fstream>
+#include <cstring>
+#include <time.h>
+#include <math.h>
 
-#include<chrono>
+#include <chrono>
 
-#define debug(x) cerr<<#x<<" = "<<x<<endl;
-#define MAX 1000
+#define debug(x) cerr << #x << " = " << x << endl;
+#define MAX 5
 
 using namespace std;
 using namespace std::chrono;
@@ -32,11 +32,11 @@ void display(int *a[], int n)
 	{
 		for (int j = 0; j < n; ++j)
 		{
-			cout<<a[i][j]<<" ";
+			cout << a[i][j] << " ";
 		}
-		cout<<endl;
+		cout << endl;
 	}
-	cout<<endl;
+	cout << endl;
 }
 
 void memset(int **a, int n)
@@ -50,7 +50,32 @@ void memset(int **a, int n)
 	}
 }
 
-void matmul(int **a, int **b, int **c, int row1, 
+void matmul_divide(int **a, int **b, int **c, int i1, int j1,
+				   int i2, int j2, int row, int col, int n)
+{
+	if (n <= 32)
+	{
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				for (int k = 0; k < n; ++k)
+					c[row + i][col + j] += a[i1 + i][j1 + k] * b[i2 + k][j2 + j];
+		return;
+	}
+
+	matmul_divide(a, b, c, i1, j1, i2, j2, row, col, n / 2);				 // A*A1
+	matmul_divide(a, b, c, i1, j1, i2, j2 + n / 2, row, col + n / 2, n / 2); //A*B1
+
+	matmul_divide(a, b, c, i1, j1 + n / 2, i2 + n / 2, j2, row, col, n / 2);				 // A*C1
+	matmul_divide(a, b, c, i1, j1 + n / 2, i2 + n / 2, j2 + n / 2, row, col + n / 2, n / 2); // B*D1
+
+	matmul_divide(a, b, c, i1 + n / 2, j1, i2, j2, row + n / 2, col, n / 2);				 // C*A1
+	matmul_divide(a, b, c, i1 + n / 2, j1, i2, j2 + n / 2, row + n / 2, col + n / 2, n / 2); // C*B1
+
+	matmul_divide(a, b, c, i1 + n / 2, j1 + n / 2, i2 + n / 2, j2, row + n / 2, col, n / 2);				 // D*C1
+	matmul_divide(a, b, c, i1 + n / 2, j1 + n / 2, i2 + n / 2, j2 + n / 2, row + n / 2, col + n / 2, n / 2); // D*D1
+}
+
+void matmul(int **a, int **b, int **c, int row1,
 			int col1, int row2, int col2, int n)
 {
 	static int i = 0, j = 0, k = 0;
@@ -65,8 +90,8 @@ void matmul(int **a, int **b, int **c, int row1,
 			c[i][j] += a[i][k] * b[k][j];
 			k++;
 
-			matmul(a, b, c, row1, col1, row2, col2, 
-					n);
+			matmul(a, b, c, row1, col1, row2, col2,
+				   n);
 		}
 
 		k = 0;
@@ -79,36 +104,22 @@ void matmul(int **a, int **b, int **c, int row1,
 	matmul(a, b, c, row1, col1, row2, col2, n);
 }
 
-// int ** matmul(int **a, int **b, int r1, int c1, int r2, int c2, int n)
-// {
-// 	int **c = new int *[n];
-// 	for (int i = 0; i < n; ++i)
-// 	{
-// 		c[i] = new int[n];
-// 	}
-// 	memset(c, n);
-// 	matmul_util(r1, c1, a, r2, cc2, b, c);
-// 	return c;
-// }
-
 int main(int argc, char const *argv[])
 {
-	ios_base::sync_with_stdio(false); 
-    srand(time(0)); 
+	ios_base::sync_with_stdio(false);
+	srand(time(0));
 	int s;
-	// clock_t start, stop;
-	// long double duration;
 	ofstream file1, file2, file3, file4;
 	file1.open("dat1.txt");
 	file2.open("dat2.txt");
 	file3.open("dat3.txt");
 	file4.open("dat4.txt");
 
-	for (int n=1; n <= 2048; n*=2)
+	for (int n = 1; n <= 8; n *= 2)
 	{
-		int **a = new int*[n];
-		int **b = new int*[n];
-		int **c = new int*[n];
+		int **a = new int *[n];
+		int **b = new int *[n];
+		int **c = new int *[n];
 		for (int i = 0; i < n; ++i)
 			a[i] = new int[n];
 		for (int i = 0; i < n; ++i)
@@ -119,8 +130,8 @@ int main(int argc, char const *argv[])
 		for (int i = 0; i < n; ++i)
 			for (int j = 0; j < n; ++j)
 			{
-				a[i][j] = rand()%MAX;
-				b[i][j] = rand()%MAX;
+				a[i][j] = rand() % MAX;
+				b[i][j] = rand() % MAX;
 			}
 		memset(c, n);
 
@@ -128,31 +139,25 @@ int main(int argc, char const *argv[])
 		for (int i = 0; i < n; ++i)
 			for (int j = 0; j < n; ++j)
 				for (int k = 0; k < n; ++k)
-					c[i][j]+=a[i][k]*b[k][j];
+					c[i][j] += a[i][k] * b[k][j];
 
 		auto stop = high_resolution_clock::now();
 		auto duration = duration_cast<microseconds>(stop - start).count();
-		file1<<setprecision(10)<<duration<<endl;
 		// display(c, n);
+		file1 << duration << endl;
 
 		// Optimization 1
 
 		memset(c, n);
-		start = high_resolution_clock::now();;
+		start = high_resolution_clock::now();
 		for (int i = 0; i < n; ++i)
-		{
 			for (int k = 0; k < n; ++k)
-			{
 				for (int j = 0; j < n; ++j)
-				{
-					c[i][j]+=a[i][k]*b[k][j];
-				}
-			}
-		}
-		stop = high_resolution_clock::now();;
+					c[i][j] += a[i][k] * b[k][j];
+		stop = high_resolution_clock::now();
 		duration = duration_cast<microseconds>(stop - start).count();
-		file2<<setprecision(10)<<duration<<endl;
 		// display(c, n);
+		file2 << duration << endl;		
 
 		// Optimization 2
 		// $ lscpu | grep cache
@@ -163,28 +168,28 @@ int main(int argc, char const *argv[])
 
 		memset(c, n);
 		s = 64;
-		start = high_resolution_clock::now();;
-		for (int i = 0; i < n; i+=s)
-			for (int j = 0; j < n; j+=s)
-				for (int k = 0; k < n; k+=s)
+		start = high_resolution_clock::now();
+		for (int i = 0; i < n; i += s)
+			for (int j = 0; j < n; j += s)
+				for (int k = 0; k < n; k += s)
 					for (int i1 = 0; i1 < min(s, n); ++i1)
 						for (int j1 = 0; j1 < min(s, n); ++j1)
 							for (int k1 = 0; k1 < min(s, n); ++k1)
-								c[i+i1][j+j1]+=a[i+i1][k+k1]*b[k+k1][j+j1];
-		stop = high_resolution_clock::now();;
+								c[i + i1][j + j1] += a[i + i1][k + k1] * b[k + k1][j + j1];
+		stop = high_resolution_clock::now();
 		duration = duration_cast<microseconds>(stop - start).count();
-		file3<<setprecision(10)<<duration<<endl;
 		// display(c, n);
+		file3 << duration << endl;
 
-		// Optimization 3
+		// Optimization 3 : Recursion
 
 		memset(c, n);
-		start = high_resolution_clock::now();;
-		matmul(a, b, c, n, n, n, n, n);
-		stop = high_resolution_clock::now();;
+		start = high_resolution_clock::now();
+		matmul_divide(a, b, c, 0, 0, 0, 0, 0, 0, n);
+		stop = high_resolution_clock::now();
 		duration = duration_cast<microseconds>(stop - start).count();
-		file4<<setprecision(10)<<duration<<endl;
 		// display(c, n);
+		file4 << duration << endl;
 	}
 	file1.close();
 	file2.close();
