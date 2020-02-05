28b9f3473a9e6b51575334d81bbc543fbd30da32
deepakgouda <deepakgouda1729@gmail.com>
2020-02-05 16:53:20 +0000

    Rename file and add documentation

--- Sorting/oddEvenSorting.c
+++ Sorting/oddEvenSorting.c
@@ -33,15 +33,14 @@ int main(int argc, char **argv)
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int n = 8, source, dest;
+    int n = world_size*2, source, dest;
     int *arr;
 
-    // Distribute data
     if (rank == 0)
     {
-
         arr = (int *)malloc(n * sizeof(int));
-
+        
+        // Initialize array
         for (int i = 0; i < n; i++)
         {
             arr[i] = n - i;
@@ -55,10 +54,10 @@ int main(int argc, char **argv)
                 if(dest)
                 {
                     int *sorted_arr;
-                    // printf("Sent data to %d\n", dest);
+                    // Send data to be sorted
                     MPI_Ssend((arr + j), 2, MPI_INT, dest, 0, MPI_COMM_WORLD);
-                    // printf("Received data from %d\n", dest);
                     sorted_arr = (int *)malloc(2*sizeof(int));
+                    // Receive sorted array
                     MPI_Recv(sorted_arr, 2, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                     arr[j] = sorted_arr[0];
                     arr[j+1] = sorted_arr[1];
@@ -69,14 +68,13 @@ int main(int argc, char **argv)
                         swap(&arr[j], &arr[j+1]);
                 }
             }
-            // printf("%d : ",i);
-            // display(arr, n);
         }
     }
     else
     {
         for (int i = 0; i < (rank==(world_size-1)?(n/2):n); i++)
         {
+            // Receive array, compare with neighbour and return array
             arr = (int *)malloc(2 * sizeof(int));
             source = 0;
             MPI_Recv(arr, 2, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -90,7 +88,6 @@ int main(int argc, char **argv)
     {
         display(arr, n);
     }
-    // printf("Process %d done\n", rank);
 
     // Finalize the MPI environment.
     MPI_Finalize();
